7cde4195aee26fe6cb01967d995fbce8df526832
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-24 20:57:23 -0500

    Reformat files

--- ecommerce-product-page-main/src/components/MobileNav/MobileNav.jsx
+++ ecommerce-product-page-main/src/components/MobileNav/MobileNav.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React from "react"
 import closeIcon from "../../images/icon-close.svg"
 import styles from "./MobileNav.module.css"
 
 function MobileNav(props) {
-    const index = props.close ? -1 : 0
+  const index = props.close ? -1 : 0
   return (
-    <div aria-hidden={props.close} className={`${styles.nav__wrapper} ${props.close ? styles.hide: styles.show}`}>
-        <nav aria-hidden={props.close} className={`${styles.nav} mobile `}>
-            <img onClick={props.toggleClose} src={closeIcon} alt="close icon" />
-            <ul className={styles.navlist}>
-                <li className={styles.navlist__item}><a tabIndex={index} href="#">Collections</a></li>
-                <li className={styles.navlist__item}><a tabIndex={index} href="#">Men</a></li>
-                <li className={styles.navlist__item}><a tabIndex={index} href="#">Women</a></li>
-                <li className={styles.navlist__item}><a tabIndex={index} href="#">About</a></li>
-                <li className={styles.navlist__item}><a tabIndex={index} href="#">Contact</a></li>
-            </ul>
-        </nav>
+    <div
+      className={`${styles.nav__wrapper} ${
+        props.close ? styles.hide : styles.show
+      }`}
+    >
+      <nav aria-hidden={props.close} className={`${styles.nav} mobile `}>
+        <img onClick={props.toggleClose} src={closeIcon} alt="close icon" />
+        <ul className={styles.navlist}>
+          <li className={styles.navlist__item}>
+            <a tabIndex={index} href="#">
+              Collections
+            </a>
+          </li>
+          <li className={styles.navlist__item}>
+            <a tabIndex={index} href="#">
+              Men
+            </a>
+          </li>
+          <li className={styles.navlist__item}>
+            <a tabIndex={index} href="#">
+              Women
+            </a>
+          </li>
+          <li className={styles.navlist__item}>
+            <a tabIndex={index} href="#">
+              About
+            </a>
+          </li>
+          <li className={styles.navlist__item}>
+            <a tabIndex={index} href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
