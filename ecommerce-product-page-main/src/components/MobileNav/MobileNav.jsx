72fea7ef8e49347cfcf23d9fb6d35fc892fc921b
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-25 02:16:10 -0500

    Import and use focus

--- ecommerce-product-page-main/src/components/MobileNav/MobileNav.jsx
+++ ecommerce-product-page-main/src/components/MobileNav/MobileNav.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useRef, useState } from "react"
 import closeIcon from "../../images/icon-close.svg"
 import styles from "./MobileNav.module.css"
+import { useFocusNav } from "../../hooks/useFocusNav"
+
 
 function MobileNav(props) {
   const navRef = useRef(null)
+  /**
+   * useFocusNav is not really being used here. It is only
+   * meant to clean up the large useEffect that is attaching
+   * and removing event listeners. Suggestions?
+   */
+  const focus = useFocusNav(navRef, props.close)
   const index = props.close ? -1 : 0
-  function toggle(e) {
-    e.key === "Escape" && !props.close && props.toggleClose()
-  }
   return (
     <div
       id="navigation"
@@ -15,7 +20,6 @@ function MobileNav(props) {
       className={`${styles.nav__wrapper} ${
         props.close ? styles.hide : styles.show
       }`}
-      onKeyDown={toggle}
     >
       <nav
         ref={navRef}
