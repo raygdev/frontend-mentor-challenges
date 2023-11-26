160267d0b0adb5250933a1996daa32b004a4fdd0
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-26 15:24:30 -0500

    Reformat useMediaQuery

--- ecommerce-product-page-main/src/hooks/useMediaQuery.js
+++ ecommerce-product-page-main/src/hooks/useMediaQuery.js
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 function useMediaQuery() {
-    const [isMobile, setIsMobile] = useState(true)
-    const [handleMediaQueryChange] = useState(() => handleChange)
-    function handleChange(e) {
-        e.matches ? setIsMobile(false) : setIsMobile(true)
-    }
-    useEffect(() => {
-
+  const [isMobile, setIsMobile] = useState(true)
+  const [handleMediaQueryChange] = useState(() => handleChange)
+  function handleChange(e) {
+    e.matches ? setIsMobile(false) : setIsMobile(true)
+  }
+  useEffect(() => {
     /**
      * use mediaQeury instead of checking for inner width for mobile
      * thanks to David Tejada for the help https://github.com/david-tejada
      */
-        const mediaQueryList = window.matchMedia("(min-width:860px)")
+    const mediaQueryList = window.matchMedia("(min-width:860px)")
 
-        if(mediaQueryList.matches) {
-            setIsMobile(false)
-        }
+    if (mediaQueryList.matches) {
+      setIsMobile(false)
+    }
 
-        mediaQueryList.addEventListener("change", handleMediaQueryChange)
+    mediaQueryList.addEventListener("change", handleMediaQueryChange)
 
-        return () => {
-            mediaQueryList.removeEventListener("change", handleMediaQueryChange)
-        }
-    },[])
+    return () => {
+      mediaQueryList.removeEventListener("change", handleMediaQueryChange)
+    }
+  }, [])
 
-    return [isMobile, setIsMobile]
+  return [isMobile, setIsMobile]
 }
 
 export default useMediaQuery
