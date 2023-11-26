84836003c03447edbdbcb80210d1284bcb6ba89b
raygdev <101662915+raygdev@users.noreply.github.com>
2023-11-26 15:21:20 -0500

    Create useMediaQuery hook

---
+++ ecommerce-product-page-main/src/hooks/useMediaQuery.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function useMediaQuery() {
+    const [isMobile, setIsMobile] = useState(true)
+    const [handleMediaQueryChange] = useState(() => handleChange)
+    function handleChange(e) {
+        e.matches ? setIsMobile(false) : setIsMobile(true)
+    }
+    useEffect(() => {
+
+    /**
+     * use mediaQeury instead of checking for inner width for mobile
+     * thanks to David Tejada for the help https://github.com/david-tejada
+     */
+        const mediaQueryList = window.matchMedia("(min-width:860px)")
+
+        if(mediaQueryList.matches) {
+            setIsMobile(false)
+        }
+
+        mediaQueryList.addEventListener("change", handleMediaQueryChange)
+
+        return () => {
+            mediaQueryList.removeEventListener("change", handleMediaQueryChange)
+        }
+    },[])
+
+    return [isMobile, setIsMobile]
+}
+
+export default useMediaQuery
